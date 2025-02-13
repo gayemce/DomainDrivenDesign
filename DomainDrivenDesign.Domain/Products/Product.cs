34b7ce49472a6f95d51ff0bd06878f6cb421288b
gayemce <100995170+gayemce@users.noreply.github.com>
2025-02-13 12:50:31 +0300

    feat(domain): add repository interfaces
    - Added `IUserRepository` to manage user persistence operations.
    - Added `IProductRepository` for product creation and retrieval.
    - Added `IOrderRepository` to handle order persistence.
    - Added `ICategoryRepository` to support category storage.

--- DomainDrivenDesign.Domain/Products/Product.cs
+++ DomainDrivenDesign.Domain/Products/Product.cs
@@ -5,13 +5,12 @@ using DomainDrivenDesign.Domain.Shared;
 namespace DomainDrivenDesign.Domain.Products;
 public sealed class Product : Entity
 {
-    public Product(Guid id, Name name, int quantity, Money price, Guid categoryId, Category category) : base(id)
+    public Product(Guid id, Name name, int quantity, Money price, Guid categoryId) : base(id)
     {
         Name = name;
         Quantity = quantity;
         Price = price;
         CategoryId = categoryId;
-        Category = category;
     }
 
     public Name Name { get; private set; }
---
+++ DomainDrivenDesign.Domain/Users/IUserRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace DomainDrivenDesign.Domain.Users;
+public interface IUserRepository
+{
+    Task CreateAsync(string name, string email, string password, string country, string city, string street, string fullAddress, string postalCode, CancellationToken cancellationToken = default);
+    Task<List<User>> GetAllAsync(CancellationToken cancellationToken = default);
+}
