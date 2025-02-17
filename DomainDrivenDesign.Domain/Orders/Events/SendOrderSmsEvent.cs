51550a507d44931d66738fb137ac4cb6724a025d
gayemce <100995170+gayemce@users.noreply.github.com>
2025-02-17 12:22:46 +0300

    feat(domain): introduce Domain Events for order and user lifecycle
    - Added OrderDomainEvent and UserDomainEvent to encapsulate domain events.
    - Implemented event handlers (SendOrderEmailEvent, SendOrderSmsEvent, SendRegisterEmailEvent) using MediatR.

---
+++ DomainDrivenDesign.Domain/Orders/Events/SendOrderSmsEvent.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace DomainDrivenDesign.Domain.Orders.Events;
+
+public sealed class SendOrderSmsEvent : INotificationHandler<OrderDomainEvent>
+{
+    public Task Handle(OrderDomainEvent notification, CancellationToken cancellationToken)
+    {
+        //SMS sending process
+        return Task.CompletedTask;
+    }
+}
---
+++ DomainDrivenDesign.Domain/Users/Events/SendRegisterEmailEvent.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace DomainDrivenDesign.Domain.Users.Events;
+
+public sealed class SendRegisterEmailEvent : INotificationHandler<UserDomainEvent>
+{
+    public Task Handle(UserDomainEvent notification, CancellationToken cancellationToken)
+    {
+        //Sending registration email for the user
+        return Task.CompletedTask;
+    }
+}
